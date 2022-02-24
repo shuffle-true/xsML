faf0a4553580858c7e42e5667271aeeb97c39d2c
LilPomidorLil <rozhinthe@ya.ru>
2022-02-24 05:35:04 +0300

    bug fixes

--- testing_model.py
+++ testing_model.py
@@ -1,15 +1,5 @@
-from tree import TreeRegressor, TreeRegressorSlow
-from sklearn.datasets import fetch_california_housing
-from sklearn.metrics import mean_squared_error as mse
-
-data = fetch_california_housing()
-X = data.data[:15000]
-y = data.target[:15000]
 
 
-model = TreeRegressor(max_depth = 3, min_samples_leaf=10)
-model.fit(X, y)
-print(mse(y, model.predict(X)))
 
 
 
